8db86f067c9a82561aa948e0b4964c20bea29b22
Erik Christensen <christensenep@gmail.com>
2013-07-10 11:19:07 -0700

    Adding comments

--- steam-badge-count.js
+++ steam-badge-count.js
@@ -1,5 +1,5 @@
+// prints a count of the total number of STEAM badges earned.
 // run on the openbadger database
-
 var steamBadges = db.badges.distinct( "_id", {"categoryAward" : { $nin: ["", null] } } );
 
 printjson(db.badgeinstances.count( {"badge" : { $in : steamBadges } } ));
