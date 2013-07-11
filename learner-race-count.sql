ce5b271374922b648f33e8242beea281d9b63f69
Erik Christensen <christensenep@gmail.com>
2013-07-11 08:51:30 -0700

    Added learner race count script

---
+++ learner-race-count.sql
@@ -0,0 +1,12 @@
+// prints a count of learners grouped by race
+// run on the csol database
+
+SELECT
+	CASE
+		WHEN raceEthnicity IS NULL OR raceEthnicity = '' THEN 'Not Specified'
+		ELSE raceEthnicity
+	END AS race,
+	COUNT(*) as count
+	FROM learners
+	GROUP BY race
+	ORDER BY FIELD(race, 'Not Specified') ASC, race;
